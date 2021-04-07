6ccd284cf56ac49218994010978c13207f9de1f6
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2021-04-07 14:44:35 -0300

    Added TimeTest.java
    A simple demonstrative class about Java time and date manipulation capabilities.

---
+++ TimeTest.java
@@ -0,0 +1,9 @@
+import java.time.*;
+public class TimeTest
+{
+    public static void main(String[] args) 
+    {
+        LocalDate myObj = LocalDate.now(); // Create a date object
+        System.out.println(myObj); // Display the current date
+    }
+}
